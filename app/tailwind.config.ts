8e570e7ce4838eb65fe2c6d77a3cb2a2190556c4
Florent <florent.assuma@gmail.com>
2026-04-13 18:23:54 +0100

    fix: extensions de config définitives

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,19 +1,7 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: {} },
   plugins: [],
 };
 export default config;
