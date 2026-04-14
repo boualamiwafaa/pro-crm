072f7624e737abaa0c857f6f49c3932e0297076d
Florent <florent.assuma@gmail.com>
2026-04-14 18:14:09 +0100

    FIX: Force Tailwind Config

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -5,9 +5,15 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
 };
